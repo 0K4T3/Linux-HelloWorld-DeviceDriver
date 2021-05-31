13d241896a2db6bd50c78e291a21b962a6b20c81
0K4T3 <o.keito317@icloud.com>
2021-05-31 23:21:37 +0900

    UPDATE: improve code

--- Makefile
+++ Makefile
@@ -4,3 +4,12 @@ obj-m := $(MODULES)
 
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+
+install:
+	insmod hello_world_driver.ko
+
+uninstall:
+	rmmod hello_world_driver
+
+clean:
+	rm ./*.ko ./*.mod* ./*.o
--- hello_world_driver.c
+++ hello_world_driver.c
@@ -1,16 +1,35 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/cdev.h>
+#include <linux/fs.h>
+
+static __init int hello_world_driver_init(void);
+static __exit void hello_world_driver_exit(void);
+int hello_world_driver_open(struct inode *inode, struct file *filp);
+int hello_world_driver_close(struct inode *inode, struct file *filp);
+
+module_init(hello_world_driver_init);
+module_exit(hello_world_driver_exit);
+MODULE_LICENSE("GPL2");
 
 static int __init hello_world_driver_init(void) {
-  printk(KERN_DEBUG, "load driver\n");
+  printk(KERN_DEBUG, "load hello world driver\n");
   printk(KERN_DEBUG, "Hello World!\n");
   return 0;
 }
 
 static void __exit hello_world_driver_exit(void) {
-  printk(KERN_DEBUG, "unload driver\n");
+  printk(KERN_DEBUG, "unload hello world driver\n");
 }
 
-module_init(hello_world_driver_init);
-module_exit(hello_world_driver_exit);
-MODULE_LICENSE("Dual BSD/GPL");
+
+int hello_world_driver_open(struct inode *inode, struct file *filp) {
+  printk(KERN_DEBUG, "open hello world dvier\n");
+  return 0;
+}
+
+int hello_world_driver_close(struct inode *inode, struct file *filp) {
+  printk(KERN_DEBUG, "close hello world dvier\n");
+  return 0;
+}
